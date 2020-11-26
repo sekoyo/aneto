01e0f86fbaf3221d38645093255b673dea5e9c08
Dominic <dominic.tobias@gmail.com>
2020-11-26 23:47:18 +0800

    Dont remove styles once added

--- src/useStyle.ts
+++ src/useStyle.ts
@@ -1,24 +1,29 @@
-import { useLayoutEffect } from 'react';
-
 const styles = new Map<string, HTMLStyleElement>();
 
-export function useStyle(uid: string, rules: string) {
-  useLayoutEffect(() => {
-    if (styles.get(uid)) {
-      return;
-    }
+export function addStyle(uid: string, rules: string) {
+  if (styles.get(uid)) {
+    return;
+  }
 
-    const style = document.createElement('style');
-    style.innerHTML = rules;
-    style.setAttribute('id', uid);
-    document.head.appendChild(style);
-    styles.set(uid, style);
+  const style = document.createElement('style');
+  style.innerHTML = rules;
+  style.setAttribute('id', uid);
+  document.head.appendChild(style);
+  styles.set(uid, style);
+}
 
-    return () => {
-      if (style && document.head.contains(style)) {
+// It's too difficult to determine when to remove styles in the case
+// of multiple components and doesn't really matter if the style hangs
+// around. So changing hook to a facade to not break compatibility.
+export const useStyle = addStyle;
+
+if ((module as any).hot) {
+  (module as any).hot.dispose(() => {
+    styles.forEach(style => {
+      if (document.head.contains(style)) {
         document.head.removeChild(style);
-        styles.delete(uid);
       }
-    };
-  }, [uid, rules]);
+    });
+    styles.clear();
+  });
 }
